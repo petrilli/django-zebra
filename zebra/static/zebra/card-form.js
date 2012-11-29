84fe06f50aa1b99ac5f55875a1495856ba95027a
Christopher Petrilli <petrilli@amber.org>
2012-11-28 21:57:35 -0500

    Checkpoint. This is nowhere near done, but it's vaguely functional. Need to finish, but want to check in so I can refactor some.

--- zebra/static/zebra/card-form.js
+++ zebra/static/zebra/card-form.js
@@ -1,33 +1,28 @@
-$(function() {
-  $("#id_card_number").parents("form").submit(function() {
-  	if ( $("#id_card_number").is(":visible")) {
-      var form = this;
-      var card = {
-        number:   $("#id_card_number").val(),
-        expMonth: $("#id_card_expiry_month").val(),
-        expYear:  $("#id_card_expiry_year").val(),
-        cvc:      $("#id_card_cvv").val()
-      };
+$(document).ready(function () {
+    $("form").submit(function (event) {
+        // Disable the button so someone doesn't hit it twice
+        $("#submit-id-submit").prop("value", "Submitting...").attr("disabled", "disabled").addClass("disabled");
 
-      Stripe.createToken(card, function(status, response) {
-        if (status === 200) {
-          // console.log(status, response);
-          $("#credit-card-errors").hide();
-          $("#id_last_4_digits").val(response.card.last4);
-          $("#id_stripe_token").val(response.id);
-          form.submit();
-		  $("button[type=submit]").attr("disabled","disabled").html("Submitting..")
-        } else {
-          $(".payment-errors").text(response.error.message);
-          $("#user_submit").attr("disabled", false);
-        }
-      });
-      
-      return false;
-      
-    } 
-    
-    return true
-    
-  });
+        Stripe.createToken(
+            {
+                number:$("#id_card_number").val(),
+                expMonth:$("#id_card_expiry_month").val(),
+                expYear:$("#id_card_expiry_year").val(),
+                cvc:$("#id_card_cvv").val()
+            },
+            stripeResponseHandler);
+        return false;
+    })
 });
+
+function stripeResponseHandler(status, response) {
+    if (response.error) {
+        $("#payment-errors").text(response.error.message);
+        $("#submit-id-submit").removeAttr("disabled").removeClass("disabled");
+    } else {
+        $("#payment-errors").hide();
+        $("#id_last_4_digits").val(response.card.last4);
+        $("#id_stripe_token").val(response.id);
+        $("form").get(0).submit();
+    }
+};
